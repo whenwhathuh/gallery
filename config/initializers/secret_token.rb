923bc57ae87cf4ee76692af5bf566dea3b14e9f7
Johnny Rutkowski <rockicestrings@gmail.com>
2013-07-07 14:05:55 -0400

    Initial Commit with Album and Photo Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gallery::Application.config.secret_token = '224d5b8116a26ee684dbc5ba53fee9efb257019fb70dd482cf59e8c2f78445150c72634e0ff115f0e1e9c2b56d7a6e84405fd7b4daee650e62bd97f1e65dbb5a'
